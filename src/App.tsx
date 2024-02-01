fd8163c1f373bb36a4ca3e5a4c68dc94aa85dab0
AlineConchetta <alineconchetta@gmail.com>
2024-01-31 23:51:48 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/NavBar"
 import Home from "./pages/home/Home"
+import Navbar from "./components/navbar/NavBar"
+import ListaCategorias from "./components/categoria/listarcategoria/ListarCategoria"
+import FormCategoria from "./components/categoria/formcategoria/FormCategoria"
+import DeletarCategoria from "./components/categoria/deletarcategoria/DeletarCategoria"
+
 
 function App() {
- 
-
   return (
     <>
-      <div className="min-h-[80vh]"> 
-      <Navbar/>
-      <Home/>
-      <Footer/>
-      </div>
+      
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+            <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      
     </>
   )
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,9 +4,9 @@ function Footer() {
 
     return (
         <>
-            <div className="flex justify-center  bg-blue-300">
+            <div className="flex justify-center  bg-green-300">
                 <div className="container flex flex-col items-center py-2">
-                    <p className='text-xl font-bold'> Farmácia </p>
+                    <p className='text-xl font-bold'> FarmaVida </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-3 py-1'>
                         <a href="" target='_blank'><FacebookLogo size={40} weight='bold' /></a>
